b424850ee698853ca6def1fe7513b5228044ecf3
Luchnik1984 <luchnik1984@mail.ru>
2025-12-06 13:32:57 +0300

    solving ticket task 5

--- src/exampleHandlingExceptions.java
+++ src/exampleHandlingExceptions.java
@@ -1,5 +1,37 @@
+import java.io.FileNotFoundException;
+
 public class exampleHandlingExceptions {
     public static void main(String[] args) {
+
+        try {
+            processUnchecked();
+
+            processChecked();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Поймали в main (checked): " + e.getMessage());
+
+        } finally {
+            System.out.println("Блок finally в main выполнен");
+        }
+
+        System.out.println("Программа завершена корректно");
+    }
+
+    static void processChecked() throws FileNotFoundException {
+        throw new FileNotFoundException("файл не найден");
+    }
+
+    static void processUnchecked() {
+        try {
+            int x = 10 / 0;
+            System.out.println("Результат: " + x);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Обработали unchecked: " + e.getMessage());
+        }
+
 
     }
 }
